ef0277c436fdcf2da099b3e2e58726eafe3c4d9e
workingjubilee <workingjubilee@gmail.com>
2019-02-18 14:35:44 -0800

    Extremely unlovely comment section exists.

--- instareact/src/components/CommentSection/CommentSection.js
+++ instareact/src/components/CommentSection/CommentSection.js
@@ -4,7 +4,10 @@ import Comment from './Comment';
 const CommentSection = props => {
   return (
     <section>
-    {props.comments.map(commentObj => {return <Comment username={commentObj.username} text={commentObj.text} />})}
+      {props.comments.map(commentObj => {return <Comment username={commentObj.username} text={commentObj.text} />})}
+      <form>
+        <input type="text"></input><button>Say it!</button>
+      </form>
     </section>
   );
 }
