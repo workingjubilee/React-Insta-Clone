fb1e9c1bac216b061fd1b22a60e1af8ec9918c04
workingjubilee <workingjubilee@gmail.com>
2019-02-20 15:01:59 -0800

    Build Login prototype.

--- reactagram/src/components/Login/Login.js
+++ reactagram/src/components/Login/Login.js
@@ -1,1 +1,13 @@
 import React from 'react';
+
+const Login = () => {
+  return (
+    <form onSubmit="">
+      <label>Name: <input type="text" name="name" /></label>
+      <label>Password: <input type="text" name="password" /></label>
+      <button>Log In</button>
+    </form>
+  )
+}
+
+export default Login;
