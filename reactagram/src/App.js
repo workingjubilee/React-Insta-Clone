5ee61f15152a4e03440d7f23a7ece8fca68c3732
workingjubilee <workingjubilee@gmail.com>
2019-02-20 14:11:09 -0800

    Finally setup PostPage

--- reactagram/src/App.js
+++ reactagram/src/App.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 // import CommentSection from './components/CommentSection/CommentSection';
-import PostContainer from './components/PostContainer/PostContainer';
-import SearchBar from './components/SearchBar/SearchBar';
+import PostsPage from './components/PostContainer/PostsPage';
 
 class App extends Component {
   constructor() {
@@ -34,11 +33,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <SearchBar searchText={this.state.searchText} searchChange={this.searchChange} />
-      {this.state.postArray.filter(postObject => {return postObject.username.includes(this.state.searchText)}).map(postObject => {return <PostContainer key={postObject.timestamp} post={postObject} likeThis={this.likeThis} />;}  )}
+        <PostsPage />
       </div>
     );
   }
 }
+
+//
+// - Authenticate will look a lot like this when you're done setting it up.
+//
+// ```js
+// const authenticate = App =>
+// class extends React.Component {
+//   render() {
+//     return <App />;
+//   }
+// };
+// ```
+
+// const withAuthenticate = App =>
+  // class extends React.Component {
+  //   render() {
+  //     return <App />;
+  //   }
+  // };
 
 export default App;
